266c1072f1f6f4911430e8a689533a7985be1ecb
vladimir0646 <sobolevbobolev@mail.ru>
2022-06-21 11:29:34 +0300

    feat: add function revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+
+   let result = [];
+   let arr = Array.from(String(n))
+   if ((arr[0] === '-')) {
+      result = (arr.slice(1));
+   } else {
+      result = arr;
+   }
+   return Number(result.reverse().join(''));
 }
+
+// console.log(reverse(-123));
+// console.log(reverse(+456789));
+// console.log(reverse(987654));
+// console.log(reverse(0102));
+
